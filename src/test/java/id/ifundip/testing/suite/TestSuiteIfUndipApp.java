b140bfe74f28fc62061549959995787fc377ae37
Jeremy Edbert Widjaja <widjajajeremy61@gmail.com>
2022-03-24 19:36:01 +0700

    Latihan

---
+++ src/test/java/id/ifundip/testing/suite/TestSuiteIfUndipApp.java
@@ -0,0 +1,14 @@
+package id.ifundip.testing.suite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSuiteIfUndipApp {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
